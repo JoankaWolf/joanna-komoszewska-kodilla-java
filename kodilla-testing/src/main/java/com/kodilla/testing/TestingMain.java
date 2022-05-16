48fefd8115320d6b49cbe156e6394c3e6a502c21
Joanna Komoszewska <joanna.komoszewska@gmail.com>
2022-05-16 12:16:00 +0200

    6.3 Zadanie: test klasy wybierajcej parzyste liczby z kolekcji

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,18 +1,7 @@
 package com.kodilla.testing;
+import com.kodilla.testing.user.SimpleUser;
 import com.kodilla.testing.calculator.Calculator;
 public class TestingMain {
     public static void main(String[] args) {
-        Calculator calculator1 = new Calculator();
-        int addAToB = calculator1.addAToB(8,6);
-        if (addAToB == 14) {
-            System.out.println("Add Test ok!");}
-        else {
-            System.out.println("Add Test failed!");}
-
-        int subtractBFromA = calculator1.subtractBFromA(8,12);
-        if (subtractBFromA == 4) {
-            System.out.println("Subtract Test ok!");}
-        else {
-            System.out.println("Subtract Test failed!");}
     }
 }
---
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -0,0 +1,46 @@
+package com.kodilla.testing.collection;
+import com.kodilla.testing.collection.OddNumbersExterminator;
+import org.junit.jupiter.api.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+@DisplayName("Test OddNumbersExterminator")
+public class CollectionTestSuite {
+    @BeforeEach
+    public void before() {
+        System.out.println("Test Case: begin");
+    }
+
+    @AfterEach
+    public void after() {
+        System.out.println("Test Case: end");
+    }
+
+    @DisplayName("I check if the class behaves correctly when the list is empty.")
+
+    @Test
+    void testOddNumbersExterminatorEmptyList() {
+        //Given
+        OddNumbersExterminator resultNumbers = new OddNumbersExterminator();
+        List<Integer> emptyList = List.of();
+        //When
+        List<Integer> exterminate1 = resultNumbers.exterminate(emptyList);
+        //Then
+        Assertions.assertEquals(0, exterminate1.size());
+        System.out.println("0" + "vs" + exterminate1);
+    }
+    @DisplayName("I check if the class behaves correctly when the list contains even and odd numbers.")
+    @Test
+    void testOddNumbersExterminatorNormalList() {
+        //Given
+        OddNumbersExterminator resultNumbersList = new OddNumbersExterminator();
+        List<Integer> numbersList = Arrays.asList(1, 5, 12, 14, 85, 74, 55);
+        List<Integer> evenNumbersList = Arrays.asList(12,14, 74);
+        //When
+        List<Integer> exterminate2 = resultNumbersList.exterminate(numbersList);
+        //Then
+        Assertions.assertEquals(evenNumbersList,exterminate2);
+        System.out.println(evenNumbersList + "vs" + exterminate2);
+    }
+}

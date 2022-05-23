8ba1fd1642f3b6f3cc9a4cec4563a4a0472da094
Joanna Komoszewska <joanna.komoszewska@gmail.com>
2022-05-23 23:50:06 +0200

    7.1 Zadanie upiekszacz tekstów

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,9 +1,10 @@
 package com.kodilla.stream;
 
-import com.kodilla.stream.lambda.Executor;
+import com.kodilla.stream.beautifier.PoemBeautifier;
 import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.lambda.Processor;
 import com.kodilla.stream.reference.FunctionalCalculator;
+
 
 public class StreamMain {
     public static void main(String[] args) {
@@ -13,7 +14,7 @@ public class StreamMain {
         ExpressionExecutor expressionExecutor = new ExpressionExecutor();       // [5]
 
         System.out.println("Calculating expressions with lambdas");
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
+        expressionExecutor.executeExpression(10, 5, Double::sum);
         expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
         expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
         expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
@@ -23,5 +24,21 @@ public class StreamMain {
         expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
         expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
         expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
+
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+        String beautyText1 = poemBeautifier.beautify("Be happy", (text-> "AAA" +" "+ text +" " + "AAA"));
+        System.out.println(beautyText1);
+        String beautyText2 = poemBeautifier.beautify("Be happy", (text-> "ABC" +" "+ text +" " + "ABC"));
+        System.out.println(beautyText2);
+        String beautyText3 = poemBeautifier.beautify("Be happy", (text-> "New world is better," +" "+ text));
+        System.out.println(beautyText3);
+        String beautyText4 = poemBeautifier.beautify("Be happy", (text-> text.toUpperCase()));
+        System.out.println(beautyText4);
+        String beautyText5 = poemBeautifier.beautify("Be happy", (text-> text.replaceFirst("Be happy", "Be worry")));
+        System.out.println(beautyText5);
+
+
+
+
     }
 }

b8ed6b9b10e09a5fa4e0a2669f7487769a68c099
Joanna Komoszewska <joanna.komoszewska@gmail.com>
2022-05-25 11:08:04 +0200

    7.3 Module

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,48 +1,21 @@
 package com.kodilla.stream;
 
-import com.kodilla.stream.beautifier.PoemBeautifier;
-import com.kodilla.stream.iterate.NumbersGenerator;
-import com.kodilla.stream.lambda.ExpressionExecutor;
-import com.kodilla.stream.lambda.Processor;
-import com.kodilla.stream.reference.FunctionalCalculator;
+import com.kodilla.stream.book.Book;
+import com.kodilla.stream.book.BookDirectory;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 
 public class StreamMain {
     public static void main(String[] args) {
-        Processor processor = new Processor();
-        processor.execute(() -> System.out.println("This is an example text."));
+        BookDirectory theBookDirectory = new BookDirectory();
+        String theResultStringOfBooks = theBookDirectory.getList().stream()  // [1]
+                .filter(book -> book.getYearOfPublication() > 2005)
+                .map(Book::toString)
+                .collect(Collectors.joining(",\n","<<",">>"));                    // [2]
 
-        ExpressionExecutor expressionExecutor = new ExpressionExecutor();       // [5]
-
-        System.out.println("Calculating expressions with lambdas");
-        expressionExecutor.executeExpression(10, 5, Double::sum);
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
-
-        System.out.println("Calculating expressions with method references");
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
-
-        PoemBeautifier poemBeautifier = new PoemBeautifier();
-        String beautyText1 = poemBeautifier.beautify("Be happy", (text-> "AAA" +" "+ text +" " + "AAA"));
-        System.out.println(beautyText1);
-        String beautyText2 = poemBeautifier.beautify("Be happy", (text-> "ABC" +" "+ text +" " + "ABC"));
-        System.out.println(beautyText2);
-        String beautyText3 = poemBeautifier.beautify("Be happy", (text-> "New world is better," +" "+ text));
-        System.out.println(beautyText3);
-        String beautyText4 = poemBeautifier.beautify("Be happy", (text-> text.toUpperCase()));
-        System.out.println(beautyText4);
-        String beautyText5 = poemBeautifier.beautify("Be happy", (text-> text.replaceFirst("Be happy", "Be worry")));
-        System.out.println(beautyText5);
-
-        System.out.println("Using Stream to generate even numbers from 1 to 20");
-        NumbersGenerator.generateEven(20);
-
-
-
-
+        System.out.println(theResultStringOfBooks);
     }
 }
